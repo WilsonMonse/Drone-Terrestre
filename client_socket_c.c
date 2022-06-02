04c8300bdcdd083ecff249f7b9e354809e4ee52f
MaximeJanez <105225362+MaximeJanez@users.noreply.github.com>
2022-06-02 17:24:21 +0200

    Create client_socket_c.c

---
+++ client_socket_c.c
@@ -0,0 +1,80 @@
+#include<stdio.h>
+#include<string.h>	//strlen
+#include<sys/socket.h>
+#include<arpa/inet.h>	//inet_addr
+
+int main(int argc , char *argv[]){
+	int socket_desc;
+	struct sockaddr_in server;
+	//char *message ;
+	char server_reply[2000];
+
+	
+	//Create socket
+	socket_desc = socket(AF_INET , SOCK_STREAM , 0);
+	if (socket_desc == -1) printf("Could not create socket");
+		
+	server.sin_addr.s_addr = inet_addr("192.168.1.103");
+	server.sin_family = AF_INET;
+	server.sin_port = htons( 8080 );
+
+	//Connect to remote server
+	if (connect(socket_desc , (struct sockaddr *)&server , sizeof(server)) < 0)
+	{
+		puts("connect error");
+		return 1;
+	}
+	
+	puts("Connected\n");
+	printf("Connected/n");
+	
+	while(1)
+	{
+		/*
+		//Send some data
+		message = "GET / HTTP/1.1\r\n\r\n";
+		if( send(socket_desc , message , strlen(message) , 0) < 0)
+		{
+			puts("Send failed");
+			return 1;
+		}
+		puts("Data Send\n")
+		*/
+	
+		//Receive a reply from the server
+		if (recv(socket_desc, server_reply , 2000 , 0) < 0)
+		{
+			puts("recv failed");
+		}
+		puts("Reply received\n");
+		puts(server_reply);
+		
+		if((server_reply[0] == 'r' && server_reply[1] == '1') || (server_reply[0] == 'r' && server_reply[1] == '0') || (server_reply[0] == 'l' && server_reply[1] == '1') || (server_reply[0] == 'l' && server_reply[1] == '0'))
+		{	
+			
+			if(server_reply[0] == 'r' && server_reply[1] == '1')
+			{
+				printf("moteur droit start");
+			}
+			if(server_reply[0] == 'r' && server_reply[1] == '0')
+			{
+				printf("moteur droit stop");
+			}
+			if(server_reply[0] == 'l' && server_reply[1] == '1')
+			{
+				printf("moteur gauche start");
+			}
+			if(server_reply[0] == 'l' && server_reply[1] == '0')
+			{
+				printf("moteur gauche stop");
+			}
+		}
+		
+		else
+		{
+			printf("message non conforme");
+		}
+		
+	}
+	return 0;
+}
