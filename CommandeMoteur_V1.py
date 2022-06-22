72cb7256dc4b06a71fdb01a0e5ca19423befcc03
MaximeJanez <105225362+MaximeJanez@users.noreply.github.com>
2022-06-22 21:24:19 +0200

    Update CommandeMoteur_V1.py

--- CommandeMoteur_V1.py
+++ CommandeMoteur_V1.py
@@ -1,8 +1,8 @@
 #**************************************************** ****************************
-#* <NOM DU FICHIER : "CommandeMoteur">
+#* <NOM DU FICHIER : "CommandeMoteur_V1">
 #*
 #* EQUIPE T-REX <NOM APPLICATION : "Commande moteur par reception client">
-#* Sortie d'origine : 07 juin 2022 
+#* Sortie d'origine : 20 juin 2022 
 #* Par : JANEZ Maxime (Communication client/Server) Ibrahim ELbahnsi (Commandes moteurs) 
 #*	
 #* Description du fichier:
@@ -12,7 +12,9 @@
 #*	
 #* Historique des modifications :
 #* 07 juin 2022 : version originale
-#* 
+#* 20 juin 2022 : mise a jour 
+#* 22 juin 2022 : harmonisation ne snake 
+#*
 #* Copyright (c) EQUIPE T-REX IUT CACHAN LP MECSE
 #* Tous les droits sont reserves.
 #****************************************************** **************************/
@@ -51,13 +53,13 @@ PORT = 8888             # port serveur
 print('programme start')
 
 #init client
-etat = 0
+sTATE = 0
 time.sleep(15)
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)      # creat socket
 print('init connexion au serveur')
 client.connect((HOST, PORT))                                    # start connexion to serveur
 print('Connexion vers ' + HOST + ':' + str(PORT) + ' reussie.')
-etat = 1
+sTATE = 1
 
 #message envoie
 ##def envoiMsg():
@@ -83,13 +85,13 @@ etat = 1
 def receptionCommande() :
         
         print ('Reception...')
-        donnees = client.recv(1024)     # receptio de la commande envoyer par le serveur
-        print ('Recu :', donnees)
+        vALUE = client.recv(1024)     # receptio de la commande envoyer par le serveur
+        print ('Recu :', vALUE)
 
         
         # demmare le moteur droit en fonction de la commande recu par le serveur
         #lorsque la gachette de la manette droite a ete presse
-        if (donnees == b'$r1'):
+        if (vALUE == b'$r1'):
             print('moteur droit start')
             #duty = 16 # marche arriere >15
             duty = 12 # marche avant < 15
@@ -97,14 +99,14 @@ def receptionCommande() :
 
         # arrete le moteur droit en fonction de la commande recu par le serveur
         #lorsque la gachette de la manette droite a ete relache
-        elif (donnees == b'$r0'):
+        elif (vALUE == b'$r0'):
             print('moteur droit stop')
             duty = 15 # largeur d'impultion permettant l'arret 
             PWM2.ChangeDutyCycle(duty)
 
         # demmare le moteur gauche en fonction de la commande recu par le serveur
         #lorsque la gachette de la manette gauche a ete presse
-        elif (donnees == b'$l1'):
+        elif (vALUE == b'$l1'):
             print('moteur gauche start')
             #duty = 16 # marche arriere >15
             duty = 12 # marche avant < 15 
@@ -112,13 +114,13 @@ def receptionCommande() :
             
         # arrete le moteur gauche en fonction de la commande recu par le serveur
         #lorsque la gachette de la manette gauche a ete relache
-        elif (donnees == b'$l0'):
+        elif (vALUE == b'$l0'):
             print('moteur gauche stop ')
             duty = 15 # largeur d'impultion permettant l'arret 
             PWM1.ChangeDutyCycle(duty)
 
         # quitte la boucle while en passant a l'etat 0 pour que le client puisse se fermer  
-        elif (donnees == b'/q'):
+        elif (vALUE == b'/q'):
             print ('close connexion')
             etat = 0
 
@@ -131,7 +133,7 @@ def receptionCommande() :
             
         
 
-while(etat == 1):
+while(sTATE == 1):
 
         # lance la fonction reception commande permettant le controle des moteurs par les manettes
         # sauf dans le cas ou la connection a ete couper dans ce cas on coupe tout les moteurs
